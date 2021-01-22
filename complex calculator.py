b6823598c6bcac1a03e76b33167cfbab2af4add6
Larfg <72176664+Larfg@users.noreply.github.com>
2021-01-22 12:43:31 -0500

    1

--- complex calculator.py
+++ complex calculator.py
@@ -1,72 +1,65 @@
-from sys import stdin
 import math
 
-def readln():
-    return stdin.readline().strip()
 
-def sum_comps(num1,num2):
+def sum_comps(num1, num2):
     res = []
     res.append(num1[0] + num2[0])
     res.append(num1[1] + num2[1])
     return res
 
-def res_comp(num1,num2):
+
+def res_comp(num1, num2):
     res = []
     res.append(num1[0] - num2[0])
     res.append(num1[1] - num2[1])
     return res
 
-def mult_comp(num1,num2):
+
+def mult_comp(num1, num2):
     res = []
-    res.append((num1[0]*num2[0])-(num1[1]*num2[1]))
-    res.append((num1[0]*num2[1])+(num2[0]*num1[1]))
+    res.append((num1[0] * num2[0]) - (num1[1] * num2[1]))
+    res.append((num1[0] * num2[1]) + (num2[0] * num1[1]))
     return res
 
-def div_comp(num1,num2):
+
+def div_comp(num1, num2):
     res = []
     a = num1[0]
     b = num1[1]
     c = num2[0]
     d = num2[1]
-    res.append(((a*c) + (b*d))/((c^2)+(d^2)))
-    res.append(((b*c) - (a*d))/((c^2)+(d^2)))
+    res.append(((a * c) + (b * d)) / ((c ^ 2) + (d ^ 2)))
+    res.append(((b * c) - (a * d)) / ((c ^ 2) + (d ^ 2)))
     return res
 
-def mod_comp(num1):
-    a = num1[0]
-    b = num1[1]
-    return math.sqrt((a^2)+(b^2))
 
-def conj_comp(num1):
-    return mult_comp(num1,[-1,0])
-
-"""""def main():
-    sel = None
-    while sel != 0:
-        print("calculadora de numeros complejos")
-        print("1) suma")
-        print("2) resta")
-        print("3) multiplicacion")
-        print("4) division")
-        print("5) modulo")
-        print("6) conjugado")
-        print("7) conversiones")
-        print("8) retornar la fase de un numero complejo")
-        print("0) salir")
-        sel = int(readln())
-        if sel == 1:
-            print("escriba la parte real del numero")
-        elif sel == 2:
-        elif sel == 3:
-        elif sel == 4:
-        elif sel == 5:
-        elif sel == 6:
-        elif sel == 7:
-        elif sel == 8:
-        elif sel == 0:
-            return 0
-        else:
-            print("ingrese un numero valido")
+def mod_comp(num):
+    a = num[0]
+    b = num[1]
+    return math.sqrt((a ^ 2) + (b ^ 2))
 
 
-main()"""""
+def conj_comp(num):
+    return mult_comp(num, [-1, 0])
+
+
+def polar_cart(num):
+    x = num[0] * math.cos(num[1])
+    y = num[0] * math.sin(num[1])
+    return round(x, 5), round(y, 5)
+
+
+def c_fase(num):
+    fase = math.atan(num[1] / num[0])
+    if (num[0] < 0 and num[1] < 0) or num[0] < 0:
+        return 180 + math.degrees(fase)
+    elif num[1] < 0:
+        return 360 + math.degrees(fase)
+    else:
+        return math.degrees(fase)
+
+
+def cart_polar(num):
+    h = mod_comp(num)
+    alpha = c_fase(num)
+    return h, alpha
